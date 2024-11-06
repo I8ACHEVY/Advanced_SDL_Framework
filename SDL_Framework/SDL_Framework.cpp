417e500952403ebde186ade32dce5d998f42f383
Kyle <159157285+I8ACHEVY@users.noreply.github.com>
2024-11-05 20:15:57 -0500

    Update SDL_Framework.cpp

--- SDL_Framework/SDL_Framework.cpp
+++ SDL_Framework/SDL_Framework.cpp
@@ -4,8 +4,14 @@
 #include <iostream>
 #include <SDL.h>
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+bool quit = false;
+
 int main(int argc, char* args[])
-{
+{   // Initialize SDL subsystem
     if (SDL_InitSubSystem(SDL_INIT_VIDEO) < 0)
     {
         std::cerr << "SDL could not init, error: " << SDL_GetError() << std::endl;
@@ -14,6 +20,56 @@ int main(int argc, char* args[])
 
     std::cout << "STL Advanced Framework\n";
 
+    window = SDL_CreateWindow(
+        "SDL Tutorial",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (window == nullptr)
+    {
+        std::cerr << "Unable to create a window. SDL_Error: "
+            << SDL_GetError() << std::endl;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr)
+    {
+        std::cerr << "Unable to get renderer. SDL_Error: "
+            << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); //Colour red
+    SDL_Event events = {};
+
+    //main game loop
+    while (!quit)
+    {   //event polling loop, while there are events inside of out events variable
+        while (SDL_PollEvent(&events))
+        {   // Handle each and every event we want to look for
+            switch (events.type)
+            {
+            case SDL_QUIT:
+                quit = true;
+                break;
+            }
+        }
+        // Draw code below
+        SDL_RenderFillRect(renderer, nullptr);
+        SDL_RenderPresent(renderer);
+
+        // Destroy renderer
+        SDL_DestroyRenderer(renderer);
+        //Destroy the window
+        SDL_DestroyWindow(window);
+
+        
+    }
+    // terminate SDL subsystems
     SDL_Quit();
     return 0;
 }
