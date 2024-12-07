404532e72fb44d6621a8e5567293768b8430c1ba
Kyle <159157285+I8ACHEVY@users.noreply.github.com>
2024-12-06 19:19:09 -0500

    Update Wasp.cpp

--- SDL_Framework/Wasp.cpp
+++ SDL_Framework/Wasp.cpp
@@ -76,3 +76,97 @@ void Wasp::CreateDivePaths() {
 	path->Sample(&sDivePaths[currentPath]);
 	delete path;
 }
+
+void Wasp::FlyInComplete() {
+	if (mDiver) {
+		mCurrentState = Dead;
+	}
+	else {
+		JoinFormation();
+	}
+}
+
+Vector2 Wasp::LocalFormationPosition() {
+	Vector2 retVal;
+
+	int dir = mIndex % 2 == 0 ? -1 : 1;
+
+	retVal.x = (sFormation->GridSize().x + sFormation->GridSize().x * 
+		2 * (mIndex / 4) * (float)dir);
+
+	retVal.y = sFormation->GridSize().y * 2 + sFormation->GridSize().y * 
+		((mIndex % 4) / 2);
+
+	return retVal;
+}
+
+void Wasp::HandleDiveState() {
+	int currentPath = mIndex % 2;
+
+	if (mCurrentWayPoint < sDivePaths[currentPath].size()) {
+		Vector2 waypointPos = mDiveStartPosition + sDivePaths
+			[mCurrentPath][mCurrentWayPoint];
+
+		Vector2 dist = waypointPos - Position();
+
+		Translate(dist.Normalized() * mSpeed * mTimer->DeltaTime(), World);
+		Rotation(atan2(dist.y, dist.x) * RAD_TO_DEG + 90.0f);
+
+		if ((waypointPos - Position()).MagnitudeSqr() < EPSILON * mSpeed / 25) {
+			mCurrentWayPoint++;
+		}
+	}
+	else {
+		Vector2 dist = WorldFormationPosition() - Position();
+
+		Translate(dist.Normalized() * mSpeed * mTimer->DeltaTime(), World);
+		Rotation(atan2(dist.y, dist.x) * RAD_TO_DEG + 90.0f);
+
+		if (dist.MagnitudeSqr() < EPSILON * mSpeed / 25) {
+			JoinFormation();
+		}
+	}
+}
+
+void Wasp::HandleDeadState() {
+
+}
+
+void Wasp::RenderDiveState() {
+	mTexture->Render();
+
+	int currentPath = mIndex % 2;
+	for (int i = 0; i < sDivePaths[currentPath].size() - 1;) {
+		Graphics::Instance()->DrawLine(
+			mDiveStartPosition.x + sDivePaths[currentPath][i].x,
+			mDiveStartPosition.y + sDivePaths[currentPath][i].y,
+			mDiveStartPosition.x + sDivePaths[currentPath][i + 1].x,
+			mDiveStartPosition.y + sDivePaths[currentPath][i + 1].y
+			);
+	}
+}
+
+Vector2 finalPos = WorldFormationPosition();
+auto currentDivePath = sDivePaths[currentPath];
+Vector2 pathEndPos = mDiveStartPosition + currentDivePath[currentDivePaths].size() - 1];
+
+Graphics::Instance()->DrawLine(
+	pathEndPos.x,
+	pathEndPos.y,
+	finalPos.x,
+	finalPos.y
+);
+
+void Wasp::RenderDeadState() {
+
+}
+
+Wasp::Wasp(int path, int index, bool challenge, bool diver) {
+	Enemy(path, index, challenge), mDiver(diver) {
+
+		mTexture = new Texture("AnimatedEnemies.png", 0, 40, 52, 40);
+		mTexture->Parent(this);
+		mTexture->Position(Vec2_Zero);
+	}
+}
+
